a6cb3ea29fec65bbd51a58a4d79bbc9fbacf00ca
Rajshree208 <46247383+Rajshree208@users.noreply.github.com>
2020-09-10 17:57:33 +0530

    Add files via upload

---
+++ VitalsCheck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VitalsAssignment
+{
+    class VitalsCheck
+    {
+
+        public bool vitalsIsOk(Alert alert, string vitalName, float value, float lower, float upper)
+        {
+
+            if (value < lower)
+            {
+                string message = "IS LOW !!";
+                alert.sendAlert(vitalName, message);
+                return false;
+            }
+            else if (value > upper)
+            {
+                string message = "IS HIGH !!";
+                alert.sendAlert(vitalName, message);
+                return false;
+            }
+            return true;
+        }
+
+        public bool vitalsAreOk(Alert alert, VitalsParameters p)
+        {
+            
+            int countOfTrueValues = 0;
+            bool vitalStatus;
+            for (int i = 0; i < p.total_no_vitals; i++)
+            {
+                string vitalname = p.vital_names[i];
+                vitalStatus = vitalsIsOk(alert, p.vital_names[i], p.vital_values[i], VitalsParameters.map_lowerlimit[vitalname], VitalsParameters.map_upperlimit[vitalname]);
+                if (vitalStatus == true)
+                {
+                    countOfTrueValues++;
+                }
+            }
+            return (countOfTrueValues == p.total_no_vitals);
+        }
+    }
+}
---
+++ VitalsParameters.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VitalsAssignment
+{
+    class VitalsParameters
+    {
+        public int total_no_vitals;
+        public string[] vital_names;
+        public float[] vital_values;
+
+        public static IDictionary<string,float> map_lowerlimit = new Dictionary<string, float>(){
+            { "BPM", 70.0f },
+            { "SPO2", 90.0f },
+            { "RESPIRATORY", 30.0f},
+            {"BLOODPRESSURE",60.0f },
+            {"HEARTRATE",90.0f }
+        };
+
+        public static IDictionary<string,float> map_upperlimit = new Dictionary<string, float>(){
+            { "BPM", 150.0f },
+            { "SPO2", float.MaxValue },
+            { "RESPIRATORY", 95.0f},
+            {"BLOODPRESSURE",150.0f },
+            {"HEARTRATE", 120.0f }
+        };
+        public VitalsParameters(int t, string[] vitalNames, float[] vitalValues)
+        {
+            total_no_vitals = t;
+            vital_names = new string[total_no_vitals];
+            vital_values = new float[total_no_vitals];
+            Array.Copy(vitalNames, vital_names, vitalNames.Length);
+            Array.Copy(vitalValues, vital_values, vitalValues.Length);
+        }
+    }
+}
